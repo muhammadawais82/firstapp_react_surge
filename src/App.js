0ad583058723c6eb71df03427e7ea95ffe8cb13b
muhammadawais82 <33548576+muhammadawais82@users.noreply.github.com>
2020-11-02 01:11:23 +0500

    changes in file and add a new commponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Meal from './meal';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Meal dishName='bryani' />
     </div>
   );
 }
---
+++ src/meal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Food(props){
+    return (
+    <div>
+        <h1>I want to eat some {props.dishName}</h1>
+    </div>
+    );
+}
+export default meal;
